fe95f994441f1584e98244e5d852b5ec0e0a61b1
Qiufeng Wang <821199012@qq.com>
2018-05-22 19:09:34 +0800

    Add files via upload

---
+++ error_test/DAM/test.py
@@ -0,0 +1,215 @@
+#coding=utf-8
+import time
+from paras import *
+from train import *
+alpha = 0.99
+
+
+
+
+ngram = BackoffModel('./origin_train_data/', './ngram_data/')
+
+def wordchangeprob(orig, updated, confusion_set):
+    prob = 0.0
+    if len(orig)==len(updated):
+        for i in range(0, len(orig)):
+            if orig[i] == updated[i]:
+                prob = prob + math.log10(alpha)
+            else:
+                noofcandidates = len(confusion_set) - 1   #要减去本身word
+                prob = prob + math.log10((1 - alpha) / noofcandidates)
+        return prob
+    else:
+        print("Original and updated sentences differ in length")
+        return prob
+
+#get probability of sentence in log base 10
+def getsentenceprob(sentence):
+    prob = 0.0
+    for i in range(0, len(sentence)-2):
+        if i == 0:
+            prob = prob + math.log10(ngram.unigram_mle(sentence[i]))
+        elif i == 1:
+            prob = prob + math.log10(ngram.bigram_back_off_model(sentence[i], sentence[i+1]))
+        else:
+            prob = prob + math.log10( ngram.trigram_back_off_model(sentence[i], sentence[i+1], sentence[i+2]) )
+    return prob
+
+def getbestfitsentence(sentence, confusion_set, i):
+    maxprob = wordchangeprob(sentence, sentence, []) + getsentenceprob(sentence)
+    best_lst = sentence[:]
+    for j in confusion_set:
+        tmp_lst = sentence[:]
+        tmp_lst[i] = j
+        tmp_prob = wordchangeprob(sentence, tmp_lst, confusion_set) + getsentenceprob(tmp_lst)
+        if tmp_prob > maxprob:
+            maxprob = tmp_prob
+            best_lst = tmp_lst[:]
+    return best_lst[i]
+
+def  main():
+    start_reading_from_files = time.time()
+    row_num = TEST_DATA_SIZE
+    with open(DATA0_PATH, 'r', encoding='utf-8') as f:
+        rows = []
+        cnt = 0
+        for line in f:
+            if (cnt < row_num):
+                rows.append(line)
+            else:
+                break
+            cnt += 1
+            if cnt%100000 == 0: print("data0: %d round" % cnt)
+        test_data0 = [one.split() for one in rows]
+
+    with open(DATA1_PATH, 'r', encoding='utf-8') as f:
+        rows = []
+        cnt = 0
+        for line in f:
+            if (cnt < row_num):
+                rows.append(line)
+            else:
+                break
+            cnt += 1
+            if cnt % 100000 == 0: print("data1: %d round" % cnt)
+        test_data1 = [one.split() for one in rows]
+
+
+    with open(DATA2_PATH, 'r', encoding='utf-8') as f:
+        rows = []
+        cnt = 0
+        for line in f:
+            if (cnt < row_num):
+                rows.append(line)
+            else:
+                break
+            cnt += 1
+            if cnt % 100000 == 0: print("data2: %d round" % cnt)
+        test_data2 = [one.split() for one in rows]
+
+    with open(DATA3_PATH, 'r', encoding='utf-8') as f:
+        rows = []
+        cnt = 0
+        for line in f:
+            if (cnt < row_num):
+                rows.append(line)
+            else:
+                break
+            cnt += 1
+            if cnt % 100000 == 0: print("data3: %d round" % cnt)
+        test_data3 = [one.split() for one in rows]
+
+    with open(TARGET_PATH, 'r', encoding='utf-8') as f:
+        test_target = []
+        cnt = 0
+        for line in f:
+            if (cnt < row_num):
+                test_target.append(line)
+            else:
+                break
+            cnt += 1
+            if cnt % 100000 == 0: print("target: %d round" % cnt)
+
+    with open(VOCAB_PATH, 'r', encoding='utf-8') as f:
+        char_set = f.read().split('\n')
+    end_reading_from_files = time.time()
+    print("===> time for reading from files: ~'%s's" % round(end_reading_from_files - start_reading_from_files))
+    cnt = 0
+    start = time.time()
+    for i in range(TEST_DATA_SIZE):
+        data1 = test_data1[i]
+        len1 = len(data1)
+        data0 = test_data0[i]
+        data2 = test_data2[i]
+        len2 = len(data2)
+        rev_data2 = data2[::-1]
+        data2 = rev_data2[0:len2 - 1]
+        len2 = len(data2)
+        sentence = data1[1:len1] + data0 + data2[0:len2]
+        original_word = int(test_data0[i][0])
+        output_word = int(getbestfitsentence(sentence, test_data3[i], len1-1))
+        target_word = int(test_target[i])
+        # 统计评估
+        statistics_evaluation(original_word, output_word, target_word)
+        cnt += 1
+        end = time.time()
+        if cnt%1000 == 0 :
+            print("testing round: %d   speed is %.1f step/s" % (cnt, (cnt/(end-start)) ) )
+            print_evaluation(TEST_RESULT_PATH)
+
+
+def statistics_evaluation(original_word, output_word, target_word):
+    global TP, FP, TN, FN, P, N, TPW, TPR
+
+    if (output_word != original_word):  # 修改的文本
+        if (original_word != target_word):  # 错改对或错改错
+            TP = TP + 1
+            if (output_word == target_word):  # 错改对
+                TPR = TPR + 1
+            if (output_word != target_word):  # 错改错
+                TPW = TPW + 1
+        elif (original_word == target_word) and (output_word != target_word):  # 对改错
+            FP = FP + 1
+    else:  # 不修改的文本
+        if (original_word == target_word):
+            TN = TN + 1
+        else:
+            FN = FN + 1
+
+
+def print_evaluation(filename):
+    global TP, FP, TN, FN, P, N, TPW, TPR
+    P = TP + FN
+    N = TN + FP
+    '''
+    print("PROOFREAD_BIAS: %.5f" % PROOFREAD_BIAS)
+    file.write("PROOFREAD_BIAS: %.5f \n" % PROOFREAD_BIAS)
+    '''
+    file = open(filename, 'a', encoding= 'utf-8')
+    print("P : %d\t N : %d" % (P,N))
+    file.write("P : %d\t N : %d\n" % (P,N))
+    print("TP : %d\t FP : %d" % (TP, FP))
+    file.write("TP : %d\t FP : %d\n" % (TP, FP))
+    print("TN : %d\t FN : %d" % (TN, FN))
+    file.write("TN : %d\t FN : %d\n" % (TN, FN))
+    print("TPR : %d\t TPW : %d" % (TPR, TPW))
+    file.write("TPR : %d\t TPW : %d\n" % (TPR, TPW))
+
+    Accuracy = (TP+TN)/(P+N)
+    Error_Rate = 1-Accuracy
+    Recall = TP/P
+    Precision = TP/(TP+FP)
+    F1_Score = 2*Precision*Recall/(Precision+Recall)
+    Correct_Recall = TPR / P
+    Correct_Precison = TPR / (TP+FP)
+    Correct_F1_Score = 2*Correct_Precison*Correct_Recall/(Correct_Precison+Correct_Recall)
+    #Specificity = TN / N
+    Delta = (P-(FP+FN+TPW)) / P
+    print("Accuracy : %.5f " % Accuracy)
+    file.write("Accuracy : %.5f \n" % Accuracy)
+    print("Error_Rate : %.5f " % Error_Rate)
+    file.write("Error_Rate : %.5f \n" % Error_Rate)
+    print("Recall : %.5f " % Recall)
+    file.write("Recall : %.5f \n" % Recall)
+    print("Precision : %.5f " % Precision)
+    file.write("Precision : %.5f \n" % Precision)
+    print("F1_Score : %.5f " % F1_Score)
+    file.write("F1_Score : %.5f \n" % F1_Score)
+    print("Correct_Recall : %.5f " % Correct_Recall)
+    file.write("Correct_Recall : %.5f \n" % Correct_Recall)
+    print("Correct_Precison : %.5f " % Correct_Precison)
+    file.write("Correct_Precison : %.5f \n" % Correct_Precison)
+    print("Correct_F1_Score : %.5f " % Correct_F1_Score)
+    file.write("Correct_F1_Score : %.5f \n" % Correct_F1_Score)
+    print("Delta : %.5f " % Delta)
+    file.write("Delta : %.5f \n\n" % Delta)
+    file.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
---
+++ error_test/DAM/train.py
@@ -0,0 +1,153 @@
+#coding=utf-8
+import os
+import random
+import math
+import time
+from collections import defaultdict
+#from utils import *
+from paras import *
+
+
+#Katz backoff smoothing technique
+TRIGRAM = "count_trigram"
+BIGRAM = "count_bigram"
+UNIGRAM = "count_unigram"
+TRIGRAM_A = "trigram_A"
+BIGRAM_A = "bigram_A"
+WORD_SET = "word_set"
+
+START_SYMBOL = "START"
+END_SYMBOL = "END"
+DISCOUNTED = 0.5
+
+def get_trained_data(LOAD_PATH):
+    print("loading '%s'..." % LOAD_PATH)
+    with open(LOAD_PATH, 'r', encoding='utf-8') as f:
+        dict = f.read()
+        return eval(dict)
+
+
+def save_to_file(content, SAVE_PATH):
+    print("saving '%s'..." % SAVE_PATH)
+    with open(SAVE_PATH, 'w', encoding='utf-8') as f:
+        f.write(str(content))
+    print("length = %d" % len(content))
+
+class BackoffModel:
+    count_trigram = {}
+    count_bigram = {}
+    count_unigram = {}
+    trigram_A = defaultdict(set)  #解决KeyError异常
+    bigram_A = defaultdict(set)
+    word_set = set()
+    total_words = 1
+
+    def __init__(self, training_corpus_dir, dict_dir):   #dict_dir数据根目录
+        self.training_corpus_dir = training_corpus_dir
+        self.dict_dir = dict_dir
+
+        self.word_set_dir = self.dict_dir + WORD_SET
+        self.trigram_dir = self.dict_dir + TRIGRAM
+        self.bigram_dir = self.dict_dir + BIGRAM
+        self.unigram_dir = self.dict_dir + UNIGRAM
+        self.trigram_A_dir = self.dict_dir + TRIGRAM_A
+        self.bigram_A_dir = self.dict_dir + BIGRAM_A
+
+        print("initializing Corpus object for '%s'..." % training_corpus_dir)
+
+        print('training...')
+        start_training = time.time()
+
+        os.makedirs(self.dict_dir)
+        for filename in os.listdir(self.training_corpus_dir):
+            with open(self.training_corpus_dir + filename, 'r+') as file:
+                count = 0
+                lines = file.readlines()
+                for line in lines:
+                     if line.strip():
+                        penult_word = START_SYMBOL
+                        last_word = START_SYMBOL
+
+                        for word in line.split():
+                            count += 1
+                            if(penult_word != START_SYMBOL):
+                                self.count_trigram[penult_word, last_word, word] = self.count_trigram.get((
+                                                                                penult_word,last_word, word), 0) + 1
+                            if(last_word != START_SYMBOL):
+                                self.count_bigram[last_word, word] = self.count_bigram.get((last_word, word), 0) + 1
+                            self.count_unigram[word] = self.count_unigram.get(word, 0) + 1
+                            if (penult_word != START_SYMBOL):
+                                self.trigram_A[penult_word, last_word].add(word)
+                            if (last_word != START_SYMBOL):
+                                self.bigram_A[last_word].add(word)
+                                self.word_set.add(word)
+
+                            penult_word = last_word
+                            last_word = word
+                            if count % 100000 == 0: print(count)
+                file.close()
+
+            end_training = time.time()
+            print('===> time for training: ~%ss' % round(end_training - start_training))
+
+            print('saving trained data to files...')
+            start_saving_to_files = time.time()
+
+            save_to_file(self.word_set, self.word_set_dir)
+            save_to_file(self.count_trigram, self.trigram_dir)
+            save_to_file(self.count_bigram, self.bigram_dir)
+            save_to_file(self.count_unigram, self.unigram_dir)
+            save_to_file(self.trigram_A, self.trigram_A_dir)
+            save_to_file(self.bigram_A, self.bigram_A_dir)
+
+            end_saving_to_files = time.time()
+            print('===> time for saving to files: ~%ss' % round(end_saving_to_files - start_saving_to_files))
+
+        self.total_words = sum(self.count_unigram.values())
+        print("num_total_words = %d" % self.total_words)
+        #if计算start就要减去self.count_unigram[START_SYMBOL]
+
+    def unigram_mle(self, word):
+        unique_count = len(self.count_unigram) - 1
+        return self.count_unigram.get(word, unique_count) / self.total_words  # TODO unknown word ???
+
+    def unigram_back_off_model(self, last_word, word):
+        unigram_mle_others = 1 - sum(self.unigram_mle(w) for w in self.bigram_A[last_word])
+        return self.bigram_alpha(last_word) * self.unigram_mle(word) / unigram_mle_others
+
+    def bigram_discounted_count(self, last_word, word):
+        return self.count_bigram[last_word, word] - DISCOUNTED
+
+    def bigram_alpha(self, last_word):
+        return 1 - sum(self.bigram_discounted_model(last_word, w) for w in self.bigram_A[last_word])
+
+    def bigram_discounted_model(self, last_word, word):
+        return self.bigram_discounted_count(last_word, word) / self.count_unigram.get(last_word, 1)
+
+    def bigram_back_off_model(self, last_word, word):
+        if word in self.bigram_A[last_word]:
+            return self.bigram_discounted_model(last_word, word)
+        else:
+            return self.unigram_back_off_model(last_word, word)
+
+    def trigram_discounted_count(self, penult_word, last_word, word):
+        return self.count_trigram[penult_word, last_word, word] - DISCOUNTED
+
+    def trigram_alpha(self, penult_word, last_word):
+        return 1 - sum(self.trigram_discounted_model(penult_word, last_word, w)
+                       for w in self.trigram_A[penult_word, last_word])
+
+    def trigram_discounted_model(self, penult_word, last_word, word):
+        return self.trigram_discounted_count(penult_word, last_word, word) / self.count_bigram[penult_word, last_word]
+
+    def trigram_back_off_model(self, penult_word, last_word, word):
+        if word in self.trigram_A[penult_word, last_word]:
+            return self.trigram_discounted_model(penult_word, last_word, word)
+        else:
+            bigram_mle_others = 1 - sum(self.bigram_back_off_model(last_word, w)
+                                        for w in self.trigram_A[penult_word, last_word])
+            return self.trigram_alpha(penult_word, last_word) * self.bigram_back_off_model(last_word, word) / \
+                bigram_mle_others
+
+if __name__ == '__main__':
+    ngram = BackoffModel('./origin_train_data/', './ngram_data/')
